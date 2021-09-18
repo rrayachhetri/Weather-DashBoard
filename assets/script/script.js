31601192b77f069242f72fddbbc5fbce0c4a2fbd
rrayachhetri <rrayachhetri@gmail.com>
2021-09-17 21:41:28 -0700

    display weather

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,26 +1,32 @@
 var citynameEl = document.querySelector("#cityname");
 var userformEl = document.querySelector("#user-form");
-var weatherContainerEL = document.querySelector("#weather-conatiner");
+var weatherContainerEL = document.querySelector("#weather-container");
+var forecastEl = document.querySelector("#forecast");
+
 
 var getWeather = function (city) {
     var apiKey = "b4d02dc986c242175b3d602f112a18eb";
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + `&appid=${apiKey}`;
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
+
             response.json().then(function (data) {
+
+                console.log(data);
                 apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=${apiKey}`;
                 fetch(apiUrl).then(function (response2) {
                     if (response2.ok) {
                         response2.json().then(function (data2) {
                             console.log(data2);
+                            // displayWeather(data, data2);
                         })
                     }
                 })
-                console.log(data, city);
-                // displayWeather(data.coord, city);
+
+                displayWeather(data, city);
             });
         } else {
-            alert("Error:Name of a City Not Found");
+            alert("Error: City Not Found");
         }
     })
         .catch(function (error) {
@@ -42,16 +48,35 @@ var submitHandler = function (event) {
 };
 userformEl.addEventListener("submit", submitHandler);
 
-// var searchHistory = function () {
-//     var currentWeather = 
-
-// }
-
-
-var currentWeather = function(data) {
+var displayWeather = function (data, data2) {
     var cityName = data.name;
-var divEl = document.createElement("div");
-divEl.textContent = cityName;
-weatherContainerEL.appendChild(divEl);
+    var icon = data.weather[0].icon;
+    var iconEl = document.createElement("p");
+    iconEl.textContent = icon;
+    var divEl = document.createElement("div");
+    var citynameH = document.createElement("h3");
+    var dateSpanEL = document.createElement("span");
+    var date = moment().format("MM/DD/YYYY");
+    dateSpanEL.textContent = date;
+    citynameH.innerHTML = cityName;
+    divEl.appendChild(iconEl);
+    citynameH.appendChild(dateSpanEL);
+    divEl.appendChild(citynameH);
+    console.log(divEl);
+    console.log(citynameH);
+    // weatherContainerEL.appendChild(citynameH);
+    // console.log(cityname);
+    // var forecast = data2.list[0];
+    // console.log(forecast);
+    // var weatherData = data.weather[0];
+    // console.log(weatherData);
+
+    // var 
+    // var 
+    // var citynameDiv = document.createElement("div");
+
+    weatherContainerEL.appendChild(divEl);
 }
-currentWeather("Albany");
+
+
+// var displayDate = 
